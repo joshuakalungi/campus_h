fe53444c43d569005549f4c99fef1993598b519f
joshuakalungi <98167896+joshuakalungi@users.noreply.github.com>
2022-02-24 22:58:19 -0800

    Delete Desktop/Backup directory

--- Desktop/Backup/campus_h-main/Desktop/Advanced/campus_h-master/android/app/src/main/kotlin/com/example/campush/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.campush
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Desktop/Backup/campus_h-main/Desktop/Advanced/campus_h-master/lib/HOME/Constants/Secrets.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  // Add your Google Maps API Key here
-  static const API_KEY = 'AIzaSyCX7vI-UAjpQsj4o2cjlm4VHKlwntoFXRs';
-}
